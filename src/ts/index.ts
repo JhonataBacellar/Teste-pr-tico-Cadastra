14ce143773ae82742f3e4d17d017e8fa790faa19
Jonatas Bacelar <129187557+JhonataBacellar@users.noreply.github.com>
2024-05-26 19:43:53 -0300

    feat:filterColor

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,9 +1,15 @@
 import { Product } from "./Product";
+import  {OpenMenuOrderMobile}  from "./menu";
+import { teste } from "@ts/teste";
 
 const serverUrl = "http://localhost:5000";
 
 function main() {
   console.log(serverUrl);
+  // console.log("ola")
+  // console.log(teste)
+  // OpenMenuOrderMobile();
+  // teste()
 }
-
 document.addEventListener("DOMContentLoaded", main);
+teste()
---
+++ src/ts/teste.ts
@@ -0,0 +1,4 @@
+export const teste = () => {
+    return console.log("Hlllo Word")
+}
+teste()
